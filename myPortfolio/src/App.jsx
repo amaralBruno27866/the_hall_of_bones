147fe8d5345bce74e531f0c936ca1419c6efc1ff
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-01-17 16:05:47 -0500

    Create: component navbar was created

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Navbar } from './frontend/components/navbar';
+
 export function App() {
   return (
     <div className="container">
-      <h1 className="mt-5">Main Page</h1>
-      <button className="btn btn-primary">Bootstrap Button</button>
+      <Navbar />
+      <h1>Hello, world!</h1>
     </div>
   );
 }
