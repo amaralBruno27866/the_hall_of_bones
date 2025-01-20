1643293899c70cdbe3122eeb5f203f8df63b6676
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-01-20 12:44:31 -0500

    Update: component navbar are done

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -6,7 +6,6 @@ export function App() {
   return (
     <div className="container">
       <Navbar />
-      <h1>Hello, world!</h1>
     </div>
   );
 }
--- myPortfolio/src/frontend/components/navbar.jsx
+++ myPortfolio/src/frontend/components/navbar.jsx
@@ -1,41 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import MyLogo from '../../assets/images/my_logo.png';
 
 export function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img src={MyLogo} alt="Logo" width="50" height="50" className="d-inline-block align-top me-2" />
+          <span>Bruno's Portfolio</span>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#about">About</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider"/></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+              <a className="nav-link" href="#education">Education</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a className="nav-link" href="#job-experience">Job Experience</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">Projects</a>
+            </li>
+            <li className="nav-item ms-3">
+              <a className="btn btn-primary" href="#contact-form">Contact</a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
       </div>
     </nav>
