5dd3752c23d15b9b7b6c6b5e2b167c2d21def249
Bruno <bamaral2.git2023@gmail.com>
2025-01-21 13:48:24 -0500

    create: component home

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { Navbar } from './frontend/components/navbar';
+import { Home } from './frontend/components/home';
 
 export function App() {
   return (
     <div className="container">
       <Navbar />
+      <Home />
     </div>
   );
 }
--- myPortfolio/src/main.jsx
+++ myPortfolio/src/main.jsx
@@ -4,6 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
+import '../src/global.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
