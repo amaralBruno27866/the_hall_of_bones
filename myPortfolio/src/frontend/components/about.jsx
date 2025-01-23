5dea7a6c9245cbf67b60c238fdcda97ead3317c4
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-01-23 14:36:48 -0500

    Update: change the card styles on About

--- myPortfolio/src/frontend/components/about.jsx
+++ myPortfolio/src/frontend/components/about.jsx
@@ -14,28 +14,13 @@ export function About() {
         <div className="row row-cols-1 row-cols-md-3 g-4">
           <div className="col">
             <div className="card h-100">
-              <img src={loading} className="card-img-top" alt="Loading..." />
+              <img src={loading} className="card-img-top" alt="..."/>
               <div className="card-body">
-                <h5 className="card-title">Card title 1</h5>
-                <p className="card-text">This is a short card.</p>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card h-100">
-              <img src={loading} className="card-img-top" alt="Loading..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title 2</h5>
-                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card h-100">
-              <img src={loading} className="card-img-top" alt="Loading..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title 3</h5>
-                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div className="card-footer">
+                <small className="text-body-secondary">Last updated 3 mins ago</small>
               </div>
             </div>
           </div>
