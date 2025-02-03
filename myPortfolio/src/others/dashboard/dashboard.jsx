447e698a0b38a451c20fe930cd0159b9992b9fdc
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-02-03 17:34:00 -0500

    Update: changing the styles for dashboard

--- myPortfolio/src/others/dashboard/dashboard.jsx
+++ myPortfolio/src/others/dashboard/dashboard.jsx
@@ -1,20 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './dashboard.module.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BsDoorOpenFill } from "react-icons/bs";
 
 export function Dashboard() {
+  const [activeLink, setActiveLink] = useState('Home');
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <div className={styles.dashboard}>
       <header className={styles.header}>
         <h1>My Dashboard</h1>
+        <BsDoorOpenFill size={30} className={styles.icon} />
       </header>
       <div className={styles.container}>
         <nav className={styles.sidebar}>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Logout</a></li>
+            <li>
+              <a
+                href="#"
+                className={activeLink === 'Home' ? styles.active : ''}
+                onClick={() => handleLinkClick('Home')}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className={activeLink === 'Profile' ? styles.active : ''}
+                onClick={() => handleLinkClick('Profile')}
+              >
+                Profile
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className={activeLink === 'About' ? styles.active : ''}
+                onClick={() => handleLinkClick('About')}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className={activeLink === 'Education' ? styles.active : ''}
+                onClick={() => handleLinkClick('Education')}
+              >
+                Education
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className={activeLink === 'Works' ? styles.active : ''}
+                onClick={() => handleLinkClick('Works')}
+              >
+                Works
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className={activeLink === 'Projects' ? styles.active : ''}
+                onClick={() => handleLinkClick('Projects')}
+              >
+                Projects
+              </a>
+            </li>
           </ul>
         </nav>
         <main className={styles.content}>
