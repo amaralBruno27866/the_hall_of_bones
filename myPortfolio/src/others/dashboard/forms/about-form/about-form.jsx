9abc257d1fef4345a32d85eb50a02965f13ed2db
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-02-04 18:45:29 -0500

    Update: schema about

--- myPortfolio/src/others/dashboard/forms/about-form/about-form.jsx
+++ myPortfolio/src/others/dashboard/forms/about-form/about-form.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import styles from './about-form.module.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BsFillPencilFill, BsFillTrashFill, BsCloudUploadFill } from "react-icons/bs";
 
 export function AboutForm() {
   return (
     <section className={styles.aboutForm}>
       <header>
         <h2>About Section Form</h2>
+        <button>Add a new content</button>
       </header>
       <div>
         Table
-      </div>
-      <div>
-        Form
       </div>
     </section>
   );
