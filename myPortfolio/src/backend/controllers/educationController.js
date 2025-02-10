e0736d81172c5e4a57d908568064bd3556d2f53c
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-02-10 17:07:13 -0500

    Update: removing admin auth and password verification

--- myPortfolio/src/backend/controllers/educationController.js
+++ myPortfolio/src/backend/controllers/educationController.js
@@ -1,15 +1,11 @@
 import Education from "../models/Education.js";
-import { verifyPassword, recordTransaction } from '../utils.js';
+import { recordTransaction } from '../utils.js';
 
 // This function will create a new education entry in the database
 export const createEducation = async (req, res) => {
   const { institution, image, url, field, degree, period, address, skill } = req.body;
 
   try {
-    if (req.user.role !== 'admin') {
-      return res.status(403).json({ message: 'Access denied. Only admins can create new education entries.' });
-    }
-
     const education = new Education({ institution, image, url, field, degree, period, address, skill });
     await education.save();
 
@@ -33,12 +29,12 @@ export const getEducations = async (req, res) => {
     console.error('Error retrieving education entries', error);
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 // This function will update an education entry
 export const updateEducation = async (req, res) => {
   const { id } = req.params;
-  const { institution, image, url, field, degree, period, address, skill, password } = req.body;
+  const { institution, image, url, field, degree, period, address, skill } = req.body;
 
   try {
     const education = await Education.findById(id);
@@ -46,9 +42,7 @@ export const updateEducation = async (req, res) => {
       return res.status(404).json({ message: 'Education entry not found' });
     }
 
-    await verifyPassword(password, req.user.password);
-
-    const oldDetails = { institution: education.institution, image: education.image, url: education.url, field: education.field, degree: education.degree, period: education.period, address: education.localization, skill: education.skill };
+    const oldDetails = { institution: education.institution, image: education.image, url: education.url, field: education.field, degree: education.degree, period: education.period, address: education.address, skill: education.skill };
     education.institution = institution || education.institution;
     education.image = image || education.image;
     education.url = url || education.url;
@@ -75,16 +69,12 @@ export const deleteEducation = async (req, res) => {
   const { id } = req.params;
 
   try {
-    if (req.user.role !== 'admin') {
-      return res.status(403).json({ message: 'Access denied. Only admins can delete education entries.' });
-    }
-
     const education = await Education.findByIdAndDelete(id);
     if (!education) {
       return res.status(404).json({ message: 'Education entry not found' });
     }
 
-    await recordTransaction(req.user, 'delete', 'education', { institution: education.institution, image: education.image, url: education.url, field: education.field, degree: education.degree, period: education.period, localization: education.localization, skill: education.skill });
+    await recordTransaction(req.user, 'delete', 'education', { institution: education.institution, image: education.image, url: education.url, field: education.field, degree: education.degree, period: education.period, address: education.address, skill: education.skill });
 
     console.log('Education entry deleted successfully', education);
     res.status(200).json({ message: 'Education entry deleted successfully' });
