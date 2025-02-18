3479889207b985c1beefd5a3c8b4ea255946d119
Bruno Amaral <141674554+amaralBruno27866@users.noreply.github.com>
2025-02-18 15:51:04 -0500

    PuT: educationDeleteModal.jsx updated

---
+++ myPortfolio/src/frontend/components/educationDeleteModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/education-form.module.css';
+
+export function EducationDeleteModal({ handleDelete, handleCancelDelete }) {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <header className={styles.modalHeader}>
+          <h2>Delete Education</h2>
+        </header>
+        <p>Are you sure that you want to delete this data?</p>
+        <div className={styles.buttonGroup}>
+          <button type="button" className={`${styles.btn} ${styles['btn-primary']}`} onClick={handleDelete}>YES</button>
+          <button type="button" className={`${styles.btn} ${styles['btn-danger']}`} onClick={handleCancelDelete}>NO</button>
+        </div>
+      </div>
+    </div>
+  )
+}
