ad9db5b3acb87373fd82dce33bf13f4b7d607c31
Bruno27866 <bamaral2.git2023@gmail.com>
2025-02-27 13:34:35 -0500

    PUT: update the component about at dashboard

---
+++ myPortfolio/middlewares/multerMiddleware.js
@@ -0,0 +1,33 @@
+import multer from 'multer';
+import path from 'path';
+
+// Configuração do armazenamento
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+// Filtro de arquivos
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png/;
+  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Limite de 5MB
+  fileFilter
+});
+
+export default upload;
