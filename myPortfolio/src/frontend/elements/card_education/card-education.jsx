5cd9d9080339d607ec811c27f108e89063e5ffdf
Bruno27866 <bamaral2.git2023@gmail.com>
2025-03-12 16:53:42 -0400

    POST: Chenging the styles for education card banner

--- myPortfolio/src/frontend/elements/card_education/card-education.jsx
+++ myPortfolio/src/frontend/elements/card_education/card-education.jsx
@@ -8,40 +8,46 @@ export function CardEducation({ educationData, handleIconClick, activeIcon }) {
   }
 
   return (
-    <section className={styles.education}>
-      <div className={styles.container}>
-        <div className={styles.image}>
-          <img src={educationData.image} alt={educationData.title} />
+    <main>
+      <figure>
+        <img src="https://via.placeholder.com/150" alt="Institution picture" />
+      </figure>
+      <article>
+        <div>
+          <h1>Institution</h1>
+          <p>Field</p>
+          <p>Degree</p>
+          <p>Start date</p>
+          <p>End date</p>
+          <p>Address</p>
         </div>
-        <h3 className={styles.title}>{educationData.title}</h3>
-        <p className={styles.text}>{educationData.description}</p>
         <hr />
-        <h5>{educationData.category}</h5>
-        <hr />
-        <h4>Technologies</h4>
-        <ul className={styles.list}>
-          {Array.isArray(educationData.technologies) && educationData.technologies.map((technology, index) => (
-            <li key={index}>{technology}</li>
-          ))}
-        </ul>
-        <hr />
-        <a href={educationData.github} target="_blank" rel="noopener noreferrer">Access the repository on GitHub</a>
-        <hr />
-        <footer className={styles.footer}>
-          <div className={styles.actions}>
-            <BsFillPencilFill
-              size={30}
-              className={`${styles.icon} ${styles['icon-pencil']} ${activeIcon?.icon === 'pencil' && activeIcon?.id === educationData._id ? styles['icon-active'] : ''}`}
-              onClick={() => handleIconClick('pencil', educationData._id)}
-            />
-            <BsFillTrashFill
-              size={30}
-              className={`${styles.icon} ${styles['icon-trash']} ${activeIcon?.icon === 'trash' && activeIcon?.id === educationData._id ? styles['icon-active'] : ''}`}
-              onClick={() => handleIconClick('trash', educationData._id)}
-            />
-          </div>
-        </footer>
-      </div>
-    </section>
+        <div>
+          <h4>Skills</h4>
+          <p>skill</p>
+          <p>skill</p>
+          <p>skill</p>
+          <p>skill</p>
+          <p>skill</p>
+        </div>
+      </article>
+      <button>
+        Link to website
+      </button>
+      <section className={styles.footer}>
+        <div className={styles.actions}>
+          <BsFillPencilFill
+            size={30}
+            className={`${styles.icon} ${styles['icon-pencil']} ${activeIcon?.icon === 'pencil' && activeIcon?.id === educationData._id ? styles['icon-active'] : ''}`}
+            onClick={() => handleIconClick('pencil', educationData._id)}
+          />
+          <BsFillTrashFill
+            size={30}
+            className={`${styles.icon} ${styles['icon-trash']} ${activeIcon?.icon === 'trash' && activeIcon?.id === educationData._id ? styles['icon-active'] : ''}`}
+            onClick={() => handleIconClick('trash', educationData._id)}
+          />
+        </div>
+      </section>
+    </main>
   )
 }
