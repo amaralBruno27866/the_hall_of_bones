8d2ba776704aabac6c3b7b249bf2d025651c9042
PC_Diego <bamaral2.git2023@gmail.com>
2025-03-25 10:33:36 -0400

    PUT: Update package-lock.jason

--- myPortfolio/server.js
+++ myPortfolio/server.js
@@ -30,7 +30,10 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected...'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
